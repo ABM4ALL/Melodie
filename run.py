6f3ea29164039c0c6e9adec3c1c895489f05f9f8
Yu <songmin.yu@isi26.isi.fhg.de>
2021-05-31 14:17:29 +0200

    minor

--- run.py
+++ run.py
@@ -8,8 +8,6 @@ from C_Model.C3_Analyzer import Analyzer
 if __name__ == "__main__":
 
     CONN = DB().create_Connection(CONS().RootDB)
-    # ID_Scenario = 3
-    for ID_Scenario in range(1, 7):
-        TableGenerator(CONN, ID_Scenario).run()
-        Model(CONN, ID_Scenario).run()
-        Analyzer(CONN, ID_Scenario).run()
+    ID_Scenario = 3
+    Model(CONN, ID_Scenario).run()
+    Analyzer(CONN, ID_Scenario).run()
