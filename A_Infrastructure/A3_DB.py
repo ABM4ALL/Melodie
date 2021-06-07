8f27d0d0c438eb95a412827498b127132581b87c
Yu <songmin.yu@isi26.isi.fhg.de>
2021-06-07 13:55:34 +0200

    minor

--- A_Infrastructure/A3_DB.py
+++ A_Infrastructure/A3_DB.py
@@ -22,13 +22,16 @@ class DB:
 
     def write_DataFrame(self, table, table_name, column_names, conn, **kwargs):
         table_DataFrame = pd.DataFrame(table, columns=column_names)
-        if len(kwargs) > 0:
+        if "dtype" in kwargs:
             table_DataFrame.to_sql(table_name, conn, index=False, if_exists='replace', chunksize=1000, dtype=kwargs["dtype"])
         else:
             table_DataFrame.to_sql(table_name, conn, index=False, if_exists='replace', chunksize=1000)
         return None
 
-
+    def revise_DataType(self, table_name, data_type, conn):
+        table_DataFrame = self.read_DataFrame(table_name, conn)
+        table_DataFrame.to_sql(table_name, conn, index=False, if_exists='replace', chunksize=1000,
+                               dtype=data_type)
 
 
 
