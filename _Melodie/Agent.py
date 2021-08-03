121997a142f3ee4468363afb9af3e3f29708f374
Yu <songmin.yu@isi26.isi.fhg.de>
2021-08-03 20:56:08 +0200

    travis added

--- _Melodie/Agent.py
+++ _Melodie/Agent.py
@@ -1,4 +1,11 @@
 
 class Agent:
 
+    def fibonacci(self, n, a=0, b=1):
+        if n == 0: # edge case
+            return a
+        if n == 1: # usual base case
+            return b
+        return self.fibonacci(n-1, b, a+b)
+
     pass
