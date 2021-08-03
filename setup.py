0776f2555893b57f0253d166f1d490051728a305
Yu <songmin.yu@isi26.isi.fhg.de>
2021-08-03 23:52:01 +0200

    readthedoc

--- setup.py
+++ setup.py
@@ -4,40 +4,34 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='abmarl',
-    version='0.1.3',
-    description='Agent Based Simulation and MultiAgent Reinforcement Learning',
+    name='abm-framework',
+    version='0.1',
+    description='A general framework that can be used to establish agent-based models for specific uses.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/llnl/abmarl',
-    author='Edward Rusu',
-    author_email='rusu1@llnl.gov',
+    url='https://github.com/SongminYu/ABM_Framework',
+    author='Songmin Yu',
+    author_email='songmin.yu@isi.fraunhofer.de',
     license='BSD 3',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
+        'Development Status :: Just started',
         'Natural Language :: English',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     project_urls={
-        'Featured Usage': 'https://abmarl.readthedocs.io/en/latest/featured_usage.html',
-        'Documentation': 'https://abmarl.readthedocs.io/en/latest/index.html',
+        'Documentation': 'https://abm-framework.readthedocs.io/en/latest/index.html',
     },
     packages=setuptools.find_packages(),
     install_requires=[
-        'tensorflow',
-        'ray[rllib]==1.4.0',
+        'numpy',
+        'pandas',
         'matplotlib',
-        'seaborn',
     ],
     python_requires='>=3.7, <3.9',
     entry_points={
         'console_scripts': [
-            'abmarl=abmarl.scripts.scripts:cli'
+            'abm-framework=abm-framework.scripts.scripts:cli'
         ]
     },
 )
