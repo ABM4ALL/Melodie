1a98b3ae7581c6ac701e4ab7dc8d3f28d76eaabd
Yu <songmin.yu@isi26.isi.fhg.de>
2021-08-04 00:55:48 +0200

    readthedoc

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='abm-framework',
+    name='melodie',
     version='0.1',
     description='A general framework that can be used to establish agent-based models for specific uses.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/SongminYu/ABM_Framework',
+    url='https://github.com/SongminYu/Melodie',
     author='Songmin Yu',
     author_email='songmin.yu@isi.fraunhofer.de',
     license='BSD 3',
@@ -24,7 +24,7 @@ setuptools.setup(
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     project_urls={
-        'Documentation': 'https://abm-framework.readthedocs.io/en/latest/index.html',
+        'Documentation': 'https://melodie.readthedocs.io/en/latest/index.html',
     },
     packages=setuptools.find_packages(),
     install_requires=[
@@ -35,7 +35,7 @@ setuptools.setup(
     python_requires='>=3.7, <3.9',
     entry_points={
         'console_scripts': [
-            'abm-framework=abm-framework.scripts.scripts:cli'
+            'melodie=melodie.scripts.scripts:cli'
         ]
     },
 )
