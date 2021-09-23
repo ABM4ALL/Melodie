367e7c8793bbcb7adf0405193fb63a3a3d04226e
hzy15610046011 <1295752786@qq.com>
2021-09-23 15:04:59 +0800

    modified test cases for ci service

--- tests/test_db.py
+++ tests/test_db.py
@@ -8,7 +8,6 @@ import os
 from Melodie.db import DB, create_db_conn
 from Melodie.config import Config
 
-
 # def test_create_db():
 #     db = DB('test')
 #     assert os.path.exists('test.sqlite')
@@ -20,19 +19,19 @@ from Melodie.config import Config
 #     db.close()
 #     # os.remove('resources/test.sqlite')
 
+cfg = Config('test', project_root=os.path.dirname(__file__), db_folder='resources/db',
+             output_folder='resources/_output')
+
 
 def test_get_scenarios():
-    scenarios = create_db_conn(
-        Config('test', db_folder='resources/db', output_folder='resources/_output')).query("select * from scenarios;")
+    scenarios = create_db_conn(cfg).query("select * from scenarios;")
 
     assert scenarios.shape[0] == 3
-    scenario_2 = create_db_conn(
-        Config('test', db_folder='resources/db', output_folder='resources/_output')).query_scenarios(id=2)
+    scenario_2 = create_db_conn(cfg).query_scenarios(id=2)
     assert scenario_2['id'][0] == 2
 
 
 def test_get_agent_results():
-    cfg = Config('test', db_folder='resources/db', output_folder='resources/_output')
     agents_df = create_db_conn(cfg).query_agent_results(scenario_id=0, id=1)
     assert agents_df.shape[0] == 200
     agents_df = create_db_conn(cfg).query_agent_results(scenario_id=0, step=1)
@@ -40,7 +39,6 @@ def test_get_agent_results():
 
 
 def test_get_env_results():
-    cfg = Config('test', db_folder='resources/db', output_folder='resources/_output')
     env_df = create_db_conn(cfg).query_env_results()
     assert env_df.shape[0] == 600
     env_df = create_db_conn(cfg).query_env_results(scenario_id=0)
