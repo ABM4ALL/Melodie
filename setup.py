87faa965d08e730372d10009641092d04d6f1eb5
hzy15610046011 <1295752786@qq.com>
2021-10-05 12:46:06 +0800

    added files

--- setup.py
+++ setup.py
@@ -33,12 +33,12 @@ setuptools.setup(
         'Natural Language :: English',
         'Operating System :: Unix',
         'Operating System :: Microsoft :: Windows'
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.6',
+        # 'Programming Language :: Python :: 3.7',
+        # 'Programming Language :: Python :: 3.8',
+        # 'Programming Language :: Python :: 3.9',
+        # 'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     project_urls={
