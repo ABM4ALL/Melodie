871c8a2ab207145889c51d67515bbf04704e4d66
SongminYu <yu.ceepcas@gmail.com>
2021-10-14 09:42:22 +0200

    Tertiary model start

---
+++ examples/TertiaryModel/TertiaryEnergyConsumption/run.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append("../..")
+# Melody package is not available on pip yet, so this example has to import Melody package placed at project root.
+# Appending project root to "sys.path" makes Melody package accessible to the interpreter.
+# This code will be removed as soon as we release the first distribution onto pip.
+
+from Melodie.config import Config
+from WealthDistribution.model.scenario import GiniScenarioManager
+from WealthDistribution.modules.agent import GINIAgent
+from WealthDistribution.model.table_generator import GiniTableGenerator
+from WealthDistribution.model.model import GiniModel
+from WealthDistribution.model.analyzer import Analyzer
+from WealthDistribution.modules.environment import GiniEnvironment
+from WealthDistribution.modules.data_collector import GiniDataCollector
+from Melodie.run import run
+
+if __name__ == "__main__":
+    run(
+        GINIAgent,
+        GiniEnvironment,
+        Config('WealthDistribution', os.path.dirname(__file__)),
+        model_class=GiniModel,
+        data_collector_class=GiniDataCollector,
+        scenario_manager_class=GiniScenarioManager,
+        table_generator_class=GiniTableGenerator,
+        analyzer_class=Analyzer
+    )
---
+++ examples/TertiaryModel/TertiaryEnergyConsumption/run_manager.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @Time: 2021/10/2 9:41
+# @Author: Zhanyi Hou
+# @Email: 1295752786@qq.com
+# @File: run_manager.py.py
+
+from Melodie.management.manager_server import run
+
+run()
