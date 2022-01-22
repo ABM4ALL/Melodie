e0fe3d83172eb08156b94635f2fb11675bb51a25
hzy15610046011 <1295752786@qq.com>
2022-01-22 15:22:08 +0800

    Modified the testcases

---
+++ Melodie/table_loader.py
@@ -0,0 +1,6 @@
+class TableLoader():
+    """
+    TableLoader loads tables
+    Simulator/Trainer/Calibrator will have reference to TableLoader for loading tables without defining tables multiple times.
+    """
+    pass
