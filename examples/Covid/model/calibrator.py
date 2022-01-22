85de546bb76952c1f4815d67b6c2beedff5f4f1d
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-01-22 14:52:52 +0100

    calibrator discussion

--- examples/Covid/model/calibrator.py
+++ examples/Covid/model/calibrator.py
@@ -1,15 +1,4 @@
-# 1. 注册一张目标表，里面的数据结构不要求
-#  - 单个数
-#  - 一条或甚至多条序列
 
-# 2. 根据表里的数据定义distance function
-#  - calibrator里面的@abstractmethod (loss, similar to fitness function in trainer, but it is opposite)
-#  - Conceptually, "distance" is opposite to "fitness", so in the GA behind, we need to convert the DanDiaoXing.
-
-
-# 这时候也注册一张scenario的表，但就只有一行了，标记待calibrate的参数，以及这些参数的范围
-# simulator跟trainer合作时候，scenarios和training_scenarios都可以有好多行（虽然后者可能只用一行），组合着train；
-# 但是，simulator跟calibrator合作的时候，scenarios和calibrating_target就只有唯一的一对一组合关系。
 import numpy as np
 
 from Melodie import Calibrator
@@ -29,7 +18,7 @@ class CovidCalibrator(Calibrator):
 
     def distance(self, environment: CovidEnvironment):
         print("infection_rate", environment.accumulated_infection / environment.current_scenario().agent_num)
-        return (environment.accumulated_infection / environment.current_scenario().agent_num - 0.5) ** 2
+        return (environment.accumulated_infection / environment.current_scenario().agent_num - 0.9) ** 2
 
     def convert_distance_to_fitness(self, distance: float):
         return 1 - distance
@@ -44,7 +33,6 @@ class CovidCalibrator(Calibrator):
         self.load_dataframe('scenarios', 'simulator_scenarios.xlsx', scenarios_dict)
 
     def register_static_dataframes(self) -> None:
-        # load由calibrator得到的表
         self.load_dataframe('calibrator_scenarios', 'calibrator_scenarios.xlsx', {})
         self.load_dataframe('calibrator_params_scenarios', 'calibrator_params_scenarios.xlsx', {})
 
