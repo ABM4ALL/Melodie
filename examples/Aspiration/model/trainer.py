497eadba14a2069c8ce4ce59d4d32d52752691fa
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2022-02-10 12:09:42 +0100

    aspiration minor update

--- examples/Aspiration/model/trainer.py
+++ examples/Aspiration/model/trainer.py
@@ -13,15 +13,20 @@ class AspirationTrainer(Trainer):
         self.add_agent_training_property('agent_list', 'strategy_param_1')
         self.add_agent_training_property('agent_list', 'strategy_param_2')
         self.add_agent_training_property('agent_list', 'strategy_param_3')
-        self.save_env_trainer_result = True
-        self.save_agent_trainer_result = True
+
+        self.add_agent_result_property('agent_list', 'sleep_count')
         self.add_agent_result_property('agent_list', 'exploration_count')
+        self.add_agent_result_property('agent_list', 'exploitation_count')
+        self.add_agent_result_property('agent_list', 'imitation_count')
 
         self.add_environment_result_property("average_technology")
+        self.add_environment_result_property("account_total")
         self.add_environment_result_property("sleep_accumulated_share")
         self.add_environment_result_property("exploration_accumulated_share")
         self.add_environment_result_property("exploitation_accumulated_share")
         self.add_environment_result_property("imitation_accumulated_share")
+        self.save_env_trainer_result = True
+        self.save_agent_trainer_result = True
         self.algorithm_cls = GeneticAlgorithm
 
     def fitness_agent(self, agent: AspirationAgent):
