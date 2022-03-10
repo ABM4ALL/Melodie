b1e7b8582980112b4e3ecfcab2045a614972fdd7
SongminYu <yu.ceepcas@gmail.com>
2022-03-10 13:36:56 +0100

    config revised a bit

--- Melodie/config.py
+++ Melodie/config.py
@@ -16,26 +16,15 @@ class Config:
                  ):
         self.project_name = project_name
         self.project_root = project_root
-        self.sqlite_folder = sqlite_folder
+        self.excel_source_folder = self.setup_folder_path(excel_source_folder)
+        self.output_folder = self.setup_folder_path(output_folder)
+        self.sqlite_folder = self.setup_folder_path(sqlite_folder)
         self.setup()
 
-        if not os.path.exists(sqlite_folder):
-            os.makedirs(sqlite_folder)
-            return
-            # raise FileNotFoundError(
-            #     f"sqlite_folder {sqlite_folder} of {self.__class__.__name__} was not found. Please create this folder.")
-
-        self.excel_source_folder = excel_source_folder
-        if not os.path.exists(excel_source_folder):
-            os.makedirs(excel_source_folder)
-            # raise FileNotFoundError(
-            #     f"excel_source_folder {excel_source_folder} of {self.__class__.__name__} was not found. Please create this folder.")
-
-        self.output_folder = output_folder
-        if not os.path.exists(output_folder):
-            os.makedirs(self.output_folder)
-            # raise FileNotFoundError(
-            #     f"output_folder {output_folder} of {self.__class__.__name__} was not found. Please create this folder.")
+    def setup_folder_path(self, folder_path):
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        return folder_path
 
     def setup(self):
         pass
