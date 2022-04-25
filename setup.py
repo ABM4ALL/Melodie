6b34edbf99ff05b523514379289dc340edc9fc77
hzy15610046011 <1295752786@qq.com>
2022-04-25 09:41:43 +0800

    Modified the problem that external args unable to be recognized

--- setup.py
+++ setup.py
@@ -1,49 +1,64 @@
+import time
+
 import setuptools
 import numpy as np
 from distutils.extension import Extension
 
-from Cython.Distutils import build_ext
-from Cython.Compiler import Options
+try:
+    from Cython.Distutils import build_ext
+    from Cython.Compiler import Options
+    import Cython
+    import numpy
+    import sys
 
-Options.annotate = True
+    print("cython version is", Cython.__version__, Cython.__file__, sys.executable, numpy.__version__)
 
-ext_modules = [
-    Extension("Melodie.boost._vectorize",
-              ["Melodie/boost/_vectorize.pyx"],
-              language="c++",
-              # extra_compile_args=["-std=c++11"]
-              ),
-    Extension("Melodie.boost._vectorize2d",
-              ["Melodie/boost/_vectorize2d.pyx"],
-              language="c++",
-              # extra_compile_args=["-std=c++11"]
-              ),
-    Extension("Melodie.boost.agent_list",
-              ["Melodie/boost/agent_list.pyx"],
-              language="c++",
-              # extra_compile_args=["-std=c++11"]
-              ),
-    Extension("Melodie.boost.grid",
-              ["Melodie/boost/grid.pyx"],
-              language="c++",
-              # extra_compile_args=["-std=c++11"],
-              ),
-    # define_macros=[('CYTHON_TRACE', '1')]),
-    Extension("Melodie.boost.fastrand",
-              ["Melodie/boost/fastrand.pyx"]),
-    Extension("Melodie.boost.basics",
-              ["Melodie/boost/basics.pyx"],
-              language="c++",
-              # extra_compile_args=["-std=c++11"]
-              ),
-    Extension("Melodie.boost.containers.container",
-              ["Melodie/boost/containers/container.pyx"],
-              language="c++",
-              # extra_compile_args=["-std=c++11"]
-              ),
-]
+    Options.annotate = True
+    ext_modules = [
+        Extension("Melodie.boost._vectorize",
+                  ["Melodie/boost/_vectorize.pyx"],
+                  language="c++",
+                  # extra_compile_args=["-std=c++11"]
+                  ),
+        Extension("Melodie.boost._vectorize2d",
+                  ["Melodie/boost/_vectorize2d.pyx"],
+                  language="c++",
+                  # extra_compile_args=["-std=c++11"]
+                  ),
+        Extension("Melodie.boost.agent_list",
+                  ["Melodie/boost/agent_list.pyx"],
+                  language="c++",
+                  # extra_compile_args=["-std=c++11"]
+                  ),
+        Extension("Melodie.boost.grid",
+                  ["Melodie/boost/grid.pyx"],
+                  language="c++",
+                  # extra_compile_args=["-std=c++11"],
+                  ),
+        # define_macros=[('CYTHON_TRACE', '1')]),
+        Extension("Melodie.boost.fastrand",
+                  ["Melodie/boost/fastrand.pyx"]),
+        Extension("Melodie.boost.basics",
+                  ["Melodie/boost/basics.pyx"],
+                  language="c++",
+                  # extra_compile_args=["-std=c++11"]
+                  ),
+        # Extension("Melodie.boost.containers.container",
+        #           ["Melodie/boost/containers/container.pyx"],
+        #           language="c++",
+        #           # extra_compile_args=["-std=c++11"]
+        #           ),
+    ]
+except:
+    import traceback
 
-ext_modules = None
+    traceback.print_exc()
+    ext_modules = None
+
+
+    def build_ext(_):
+        return print(
+            'Cython was not installed. With cython you may get better peformance boost!')
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -90,7 +105,8 @@ setuptools.setup(
         'websockets',
         'sqlalchemy',
         'astunparse',
-        'pprintast'
+        'pprintast',
+        'cython==3.0.0a10'
     ],
     python_requires='>=3.5',
     entry_points={
