f4d9093ea5673d6667d162f1c5dfbbcc9df9cdd8
hzy15610046011 <1295752786@qq.com>
2022-08-08 16:59:06 +0800

    updated gitignore

--- Melodie/network.py
+++ Melodie/network.py
@@ -39,16 +39,10 @@ class Network:
     def __init__(self, edge_cls: Type[Edge] = None, directed=False):
         self.simple = True
         self.directed = directed
-        self._nodes: Set[NodeType] = set()
-        self._edges: Dict[NodeType, Dict[NodeType, Edge]] = {}
-        # self._adj: Dict[int, Dict[int, Edge]] = {}
-        # self._agent_ids: Dict[
-        #     int, Dict[int, Set[int]]
-        # ] = {}  # {category_id: {0 : set(1, 2, 3)}}
-        # self._agent_pos: Dict[int, Dict[int, int]] = {}  # {'wolves': {0: 1}}
+        self.nodes: Set[NodeType] = set()
+        self.edges: Dict[NodeType, Dict[NodeType, Edge]] = {}
         self.edge_cls: ClassVar[Edge] = edge_cls if edge_cls is not None else Edge
         self.setup()
-        self._nx_edges = None
 
     def _setup(self):
         self.setup()
@@ -64,28 +58,22 @@ class Network:
         :param edge
         :return:
         """
-        if source_id not in self._edges:
-            self._edges[source_id] = {}
-        self._edges[source_id][target_id] = edge
+        if source_id not in self.edges:
+            self.edges[source_id] = {}
+        self.edges[source_id][target_id] = edge
         if not self.directed:
-            if target_id not in self._edges:
-                self._edges[target_id] = {}
-            self._edges[target_id][source_id] = edge
+            if target_id not in self.edges:
+                self.edges[target_id] = {}
+            self.edges[target_id][source_id] = edge
 
-    def get_edge_directed(self, source_id: NodeType, target_id: NodeType):
+    def get_edge(self, source_id: NodeType, target_id: NodeType):
         """
         Get an edge from the network
         :param source_id:
         :param target_id:
         :return:
         """
-        return self._edges[source_id][target_id]
-
-    def get_edge(self, source_id: NodeType, target_id: NodeType):
-        try:
-            self.get_edge_directed(source_id, target_id)
-        except KeyError:
-            self.get_edge_directed(target_id, source_id)
+        return self.edges[source_id][target_id]
 
     def remove_edge(self, source_id: NodeType, target_id: NodeType):
         """
@@ -95,12 +83,12 @@ class Network:
         :param target_id:
         :return:
         """
-        self._edges[source_id].pop(target_id)
+        self.edges[source_id].pop(target_id)
         if not self.directed:
-            self._edges[target_id].pop(source_id)
+            self.edges[target_id].pop(source_id)
 
     def get_neighbor_positions(self, agent_id: int, category: int) -> List[Tuple[int, int]]:
-        neighbor_ids = self._edges[(category, agent_id)]
+        neighbor_ids = self.edges[(category, agent_id)]
         if neighbor_ids is None:
             return []
         else:
@@ -119,7 +107,7 @@ class Network:
         :return:
         """
         agent_tuple: NodeType = (category, agent_id)
-        self._nodes.add(agent_tuple)
+        self.nodes.add(agent_tuple)
 
     def _remove_agent(self, category: int, agent_id: int):
         """
@@ -129,11 +117,11 @@ class Network:
         :return:
         """
         agent_tuple = (category, agent_id)
-        self._nodes.remove(agent_tuple)
-        target_edges = self._edges.pop(agent_tuple)
+        self.nodes.remove(agent_tuple)
+        target_edges = self.edges.pop(agent_tuple)
         if not self.directed:
             for target_node, edge in target_edges.items():
-                self._edges[target_node].pop(agent_tuple)
+                self.edges[target_node].pop(agent_tuple)
 
     def remove_agent(self, agent: Agent):
         self._remove_agent(agent.category, agent.id)
@@ -157,15 +145,7 @@ class Network:
         self.add_edge(src_pos, dst_pos, edge)
 
     def all_agents(self) -> Set[NodeType]:
-        return self._nodes
-
-    # def all_agent_on_node(self, node_id) -> List[Tuple[int, int]]:
-    #     l = []
-    #     for category_id, ids_in_category in self._agent_ids.items():
-    #         agent_ids = ids_in_category.get(node_id)
-    #         if agent_ids is not None:
-    #             l.extend([(category_id, agent_id) for agent_id in agent_ids])
-    #     return l
+        return self.nodes
 
     def setup_agent_connections(self,
                                 agent_lists: List[AgentList],
@@ -186,7 +166,7 @@ class Network:
                 node_id += 1
 
         g = getattr(nx, network_type)(
-            len(self._nodes),
+            len(self.nodes),
             **network_params,
         )
 
@@ -232,16 +212,16 @@ class Network:
                 node_id += 1
 
         if builder is not None:
-            g = builder(self._nodes)
+            g = builder(self.nodes)
         else:
             if network_name == "barabasi_albert_graph":
                 g = nx.__getattribute__(network_name)(
-                    len(self._nodes),
+                    len(self.nodes),
                     **network_params,
                 )
             elif network_name == "watts_strogatz_graph":
                 g = nx.__getattribute__(network_name)(
-                    len(self._nodes),
+                    len(self.nodes),
                     **network_params,
                 )
             else:
