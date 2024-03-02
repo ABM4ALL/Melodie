382153f0a071f6154698210895d02133b1f7ba96
hzy14610046011 <1295752786@qq.com>
2024-03-02 12:25:18 +0800

    modified pandas version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
     install_requires=scientific_toolchain_versions()
     + [
         "chardet",
-        "pandas~=2.1",
+        "pandas<=2.1",
         "seaborn",
         "networkx",
         "openpyxl",
